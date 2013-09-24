5012eb1167178b996d3e66bfdae195cf85f9191a
Rick Veens <rickveens92@gmail.com>
2013-09-24 12:35:56 +0200

    Added .project file and gui interface.

---
+++ src/main/java/nl/avans/IGUI.java
@@ -0,0 +1,23 @@
+package src.main.java.nl.avans;
+
+public interface IGUI {
+	public int getCurrentValue(int x, int y);
+
+	public void setCurrentValue(int x, int y, int currentValue);
+
+	public int getSolutionValue(int x, int y);
+
+	public int[][] getSubRegion(int subregion);
+
+	public int[] getColumn(int column);
+
+	public int[] getRow(int row);
+
+	public int getNumberOfColumns();
+
+	public int getNumberOfRows();
+
+	public int getSize();
+
+	public EDifficulty getDifficulty();
+}
